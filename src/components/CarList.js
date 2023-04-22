c319aaa54c1ee661e95006b4b2bc5ec64ac1a6d1
Hao Chen <hche8093@uni.sydney.edu.au>
2023-04-22 13:01:06 +1000

    Add search highlight

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -3,20 +3,33 @@ import { removeCar } from '../store'
 
 function CarList() {
 	const dispatch = useDispatch()
-	const cars = useSelector(({ cars: { cars, searchTerm } }) => {
-		// * Derived state
-		return cars.filter((car) =>
-			car.name.toLowerCase().includes(searchTerm.toLowerCase())
-		)
-	})
+	const { cars, name } = useSelector(
+		({ form, cars: { cars, searchTerm } }) => {
+			// * Derived state
+			const filteredCars = cars.filter((car) =>
+				car.name.toLowerCase().includes(searchTerm.toLowerCase())
+			)
+
+			return {
+				cars: filteredCars,
+				name: form.name,
+			}
+		}
+	)
 
 	const handleCarDelete = (car) => {
 		dispatch(removeCar(car.id))
 	}
 
 	const renderedCars = cars.map((car) => {
+		// Decide if this car should be highlighted
+		const bold = name && car.name.toLowerCase().includes(name.toLowerCase())
+
 		return (
-			<div key={car.id} className='panel'>
+			<div
+				key={car.id}
+				className={`panel ${bold && 'bold has-background-info-light'}`}
+			>
 				<p>
 					{car.name} - ${car.cost}
 				</p>
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux'
 import { store } from './store'
 import App from './App'
 
-import 'bulma/css/bulma.min.css'
+import 'bulma/css/bulma.css'
 import './styles.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
