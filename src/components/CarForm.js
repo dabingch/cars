f9a53dcac9877647b82ea6c345354d27422c58d2
Hao Chen <hche8093@uni.sydney.edu.au>
2023-04-22 11:51:23 +1000

    Add submit in form

--- src/components/CarForm.js
+++ src/components/CarForm.js
@@ -1,20 +1,35 @@
 import { useDispatch, useSelector } from 'react-redux'
-import { changeName } from '../store'
+import { changeName, changeCost, addCar } from '../store'
 
 function CarForm() {
 	const dispatch = useDispatch()
-	const name = useSelector((state) => {
-		return state.form.name
+	const { name, cost } = useSelector((state) => {
+		return {
+			name: state.form.name,
+			cost: state.form.cost,
+		}
 	})
+
+	const handleSubmit = (event) => {
+		event.preventDefault()
+
+		dispatch(addCar({ name, cost }))
+	}
 
 	const handleNameChange = (event) => {
 		dispatch(changeName(event.target.value))
 	}
 
+	const handleCostChange = (event) => {
+		// ! event.target.value is a string, so we need to convert it to a number
+		const carCost = parseInt(event.target.value) || 0
+		dispatch(changeCost(carCost))
+	}
+
 	return (
 		<div className='car-form panel'>
 			<h4 className='subtitle is-3'>Add Car</h4>
-			<form>
+			<form onSubmit={handleSubmit}>
 				<div className='field-group'>
 					<div className='field'>
 						<label className='label'>Name</label>
@@ -24,6 +39,19 @@ function CarForm() {
 							className='input is-expanded'
 						/>
 					</div>
+
+					<div className='field'>
+						<label className='label'>Cost</label>
+						<input
+							value={cost || ''}
+							onChange={handleCostChange}
+							className='input is-expanded'
+							type='number'
+						/>
+					</div>
+				</div>
+				<div className='field'>
+					<button className='is-link'>Submit</button>
 				</div>
 			</form>
 		</div>
